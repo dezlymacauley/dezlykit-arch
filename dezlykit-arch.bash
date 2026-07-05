99241d4eaab9607322932590b7e363bf1c52f7f9
dezlymacauley <dezlymacauley@proton.me>
2026-07-05 19:54:09 +0200

    Added Rust setup

---
+++ dezlykit-arch.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Get directory where this script lives
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run the bash scripts in the `installation-scripts` directory
+bash "$SCRIPT_DIR/installation-scripts/01_rust.bash"
---
+++ installation-scripts/01_rust.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm rustup
+
+rustup update stable
+
+rustup default stable
+
+rustup component add --toolchain stable rust-analyzer
+
+if command -v cargo-binstall >/dev/null 2>&1; then
+    cargo binstall cargo-binstall
+else
+    cargo install cargo-binstall --locked
+fi
