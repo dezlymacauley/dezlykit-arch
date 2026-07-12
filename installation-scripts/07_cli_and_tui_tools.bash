64ea41739ff61d0fcd18012b983ac539ad1d9219
dezlymacauley <dezlymacauley@proton.me>
2026-07-12 11:40:57 +0200

    Added Starship config

--- installation-scripts/07_cli_and_tui_tools.bash
+++ installation-scripts/07_cli_and_tui_tools.bash
@@ -4,6 +4,7 @@ sudo pacman -S --needed --noconfirm fish
 sudo pacman -S --needed --noconfirm neovim
 sudo pacman -S --needed --noconfirm zellij
 sudo pacman -S --needed --noconfirm yazi
+sudo pacman -S --needed --noconfirm starship
 
 sudo pacman -S --needed --noconfirm tree
 sudo pacman -S --needed --noconfirm bat
